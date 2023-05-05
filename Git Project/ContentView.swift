a05c1d7aa6aca3c15a0025eae2a3c4aec83e1566
Dmitri Morozov <dmitri.m80@gmail.com>
2023-05-05 15:50:46 -0400

    Initial Commit

---
+++ Git Project/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Git Project
+//
+//  Created by Dmitri Morozov on 5/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isPresented = false
+    var body: some View {
+        NavigationView {
+            Button("Show Modal") {
+                self.isPresented = true
+            }
+            .sheet(isPresented: $isPresented){
+                Text("This is modal screen")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Git Project/Git_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_ProjectApp.swift
+//  Git Project
+//
+//  Created by Dmitri Morozov on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Git_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
